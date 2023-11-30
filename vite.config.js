69055921e4ea8e8e10b6c7f06c7e8fce9df61a04
Yonela Johannes <johannesyonela@gmail.com>
2023-11-30 23:20:35 +0200

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist', // Ensure this points to the correct output directory
+  },
 });
