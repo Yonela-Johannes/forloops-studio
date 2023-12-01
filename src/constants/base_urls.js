aef7c2bed27e37517fd1d21a74e49005177d6ed4
Yonela Johannes <johannesyonela@gmail.com>
2023-12-01 10:41:23 +0200

    env link

--- src/constants/base_urls.js
+++ src/constants/base_urls.js
@@ -3,7 +3,7 @@ let apiUrl;
 if (process.env.NODE_ENV === 'production') {
   apiUrl = 'https://forloops-studio.vercel.app/';
 } else {
-  apiUrl = 'http://localhost:4000/api/';
+  apiUrl = 'https://mysite-server-ruddy.vercel.app/';
 }
 
 export const baseUrl = apiUrl;
